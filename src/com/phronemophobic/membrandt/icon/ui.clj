863aff3eee14cb99a50ff085691e189351d32508
Adrian Smith <smith.adriane@gmail.com>
2025-03-14 11:35:59 -0700

    add :on-click attribute to icon ui.

--- src/com/phronemophobic/membrandt/icon/ui.clj
+++ src/com/phronemophobic/membrandt/icon/ui.clj
@@ -7,20 +7,28 @@
             [com.phronemophobic.membrandt.icon.impl.common :as icon-common]))
 
 (def icon-size [18 18])
-(defui icon [{:keys [name size hover?]}]
+(defui icon [{:keys [name size hover? on-click]}]
   (let [primary-color (if hover?
                         "#1677ff"
                         "#555555")
         secondary-color (if hover?
                           "#1677ff"
-                          "#555555")]
-    (basic/on-hover
-     {:hover? hover?
-      :$body nil
-      :body
-      (skia/svg
-       (icon-common/use-colors
-        (icon/svg-str name "outlined")
-        primary-color
-        secondary-color)
-       (or size icon-size))})))
+                          "#555555")
+        elem (basic/on-hover
+              {:hover? hover?
+               :$body nil
+               :body
+               (skia/svg
+                (icon-common/use-colors
+                 (icon/svg-str name "outlined")
+                 primary-color
+                 secondary-color)
+                (or size icon-size))})
+        elem (if on-click
+               (ui/on
+                :mouse-down
+                (fn [_]
+                  (on-click))
+                elem)
+               elem)]
+    elem))
